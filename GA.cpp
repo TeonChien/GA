dcf798a288e15389b981841e16f6af5907d12227
TeonChien <teon0327@hotmail.com>
2017-10-24 21:11:44 +0800

    10/24 OK

--- GA.cpp
+++ GA.cpp
@@ -6,11 +6,12 @@
 #include <iomanip>
 #include <stdlib.h>
 
+
 #define parent_num 6    //母群個數
 #define data_num 150    //資料數
 #define dimension 4     //資料維度
 #define group_num 3     //分幾群
-#define mutation_num 5  //突變要換的點數量
+#define mutation_num 2  //突變要換的點數量
 using namespace std;
 
 vector<vector<float> > Dataset;     //存讀到的資料
@@ -19,7 +20,9 @@ int parent[parent_num][data_num];   //母群
 int choice[parent_num][data_num];   //選擇群
 int children[parent_num][data_num];   //子群
 float SSE[parent_num];
-float group_center[group_num][dimension];     //群中心
+float group_center[parent_num][group_num][dimension];     //群中心
+float crossover_rate=0.9;
+float mutation_rate=0.03;
 int circulartimes=1000;
 
 void read_file() {//char filename[FILENAME_MAX];
@@ -55,7 +58,6 @@ void bubbleSortIncrease(float a[],int b[], int len)
             }
         }
     }
-
 }
 
 void bubbleSortDecrease(int a[], int len)
@@ -69,6 +71,37 @@ void bubbleSortDecrease(int a[], int len)
                 a[j + 1] = temp;
             }
         }
+    }
+
+}
+
+void group_find(int z) {
+
+    for (int i = 0; i < data_num; i++) {
+        int min = 9999;
+
+        for (int j = 0; j < group_num; j++) {
+            float total = 0;
+            float distance = 0;
+            for (int k = 0; k < dimension; k++) {
+                distance = (Dataset[i][k] - group_center[z][j][k]) * (Dataset[i][k] - group_center[z][j][k]);
+                total += distance;
+            }
+            if (total < min) {
+                min = total;
+                parent[z][i] = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < group_num; i++) {
+        vector<int> row;
+        for (int j = 0; j < data_num; j++) {
+            if (parent[z][j] == i) {
+                row.push_back(j);
+            }
+        }
+        Groupset.push_back(row);
     }
 
 }
@@ -88,50 +121,55 @@ void center_find(int arr[parent_num][data_num],int z) {
             }
         }
         Groupset.push_back(row);
+
         for (int n = 0; n < dimension; n++) {
-            group_center[k][n] = (total[n] / count);
+            if(total[n]&&count==0){
+                group_center[z][k][n]=0;
+            }
+            group_center[z][k][n] = (total[n] / count);
         }
     }
+
 }
 
-float distance_cal(float total, int i, int j) {
+float distance_cal(float total, int i, int j,int z) {
     float distance = 0;
     for (int k = 0; k < dimension; k++) {
-        distance += (Dataset[Groupset[i][j]][k] - group_center[i][k]) *
-                    (Dataset[Groupset[i][j]][k] - group_center[i][k]);
+        distance += (Dataset[Groupset[i][j]][k] - group_center[z][i][k]) *
+                    (Dataset[Groupset[i][j]][k] - group_center[z][i][k]);
     }
     total += distance;
     return total;
 }
 
 void SSE_cal(int z) {
-    float total=0;
+    float total = 0;
     for (int i = 0; i < group_num; i++) {
         for (int j = 0; j < Groupset[i].size(); j++) {
-            total = distance_cal(total, i, j);
+            total = distance_cal(total, i, j, z);
         }
     }
-
     SSE[z] = total;
+
 }
 
-int parent_choice(const int *fitness, int fitness_total) {
+int parent_choice(const int *fitness, int total) {
     int index;
-    int random_number=rand()%fitness_total;
+    int random_number=rand()%total;
 
     if(random_number>=0&&random_number<fitness[0]){
         index=0;
     }
-    else if(random_number>=fitness[0]&&random_number<fitness[1]){
+    else if(random_number>=fitness[0]&&random_number<fitness[0]+fitness[1]){
         index=1;
     }
-    else if(random_number>=fitness[1]&&random_number<fitness[1]+fitness[2]){
+    else if(random_number>=(fitness[0]+fitness[1])&&random_number<(fitness[0]+fitness[1]+fitness[2])){
         index=2;
     }
-    else if(random_number>=fitness[1]+fitness[2]&&random_number<fitness[1]+fitness[2]+fitness[3]){
+    else if(random_number>=(fitness[0]+fitness[1]+fitness[2])&&random_number<(fitness[0]+fitness[1]+fitness[2]+fitness[3])){
         index=3;
     }
-    else if(random_number>=fitness[1]+fitness[2]+fitness[3]&&random_number<fitness[1]+fitness[2]+fitness[3]+fitness[4]){
+    else if(random_number>=(fitness[0]+fitness[1]+fitness[2]+fitness[3])&&random_number<(fitness[0]+fitness[1]+fitness[2]+fitness[3]+fitness[4])){
         index=4;
     }
     else{
@@ -141,35 +179,28 @@ int parent_choice(const int *fitness, int fitness_total) {
 }
 
 void initial() {
-    for (int i = 0; i < parent_num; i++) {      //隨機產生母群的解
-        for (int j = 0; j < data_num; j++) {
-            parent[i][j] = rand() % group_num;
-        }
-    }
-    cout<<"parent"<<endl;
-    for (int z = 0; z < parent_num; z++) {     //產生母群的SSE
-        center_find(parent,z);     //群中心
-        SSE_cal(z);         //計算SSE
-
-//        for(int a=0;a <data_num;a++){
-//            cout<<setw(4)<<a<<"["<<parent[z][a]<<"] ";
-//        }
-        cout << setprecision(5) << "[" << z << "] SSE=" << SSE[z] << endl;
-    }
-}
-
-void reproduction() {
     int index_sort[parent_num]={0,1,2,3,4,5};
     int fitness[parent_num];
     int fitness_sort[parent_num];
     float total=0;
     int fitness_total=0;
+    for (int i = 0; i < parent_num; i++) {      //隨機產生母群的解
+        for (int j = 0; j < group_num; j++) {
+            int group_rand = rand() % data_num;
+            for(int k=0;k<dimension;k++){
+                group_center[i][j][k] = Dataset[group_rand][k];
+            }
+        }
+    }
 
     cout<<"parent"<<endl;
-    for (int z = 0; z < parent_num; z++) {     //產生母群的SSE
-        center_find(parent,z);     //群中心
+    //產生母群的SSE
+    for (int z = 0; z < parent_num; z++) {
+        group_find(z);
         SSE_cal(z);         //計算SSE
         total+=SSE[z];
+        Groupset.clear();
+
 //        for(int a=0;a <data_num;a++){
 //            cout<<setw(4)<<a<<"["<<parent[z][a]<<"] ";
 //        }
@@ -185,7 +216,60 @@ void reproduction() {
         fitness[index_sort[i]]=fitness_sort[i];
     }
     for(int i=0;i<parent_num;i++){
-        cout<<"["<<i<<"] fitness="<<fitness[i]<<endl;
+//        cout<<"["<<i<<"] fitness="<<fitness[i]<<endl;
+        fitness_total+=fitness[i];
+    }
+
+    for(int i=0;i<parent_num;i++){
+        int index = parent_choice(fitness, fitness_total);
+        for(int j=0;j<data_num;j++){
+            choice[i][j]=parent[index][j];
+        }
+    }
+    /*
+    for (int i = 0; i < parent_num; i++) {
+        cout<<"Choice "<<i<<endl;
+        for (int j = 0; j < data_num; j++) {
+            cout<<setw(4)<<j<<"["<<choice[i][j]<<"] ";
+        }
+    }
+    */
+}
+
+void reproduction() {
+    int index_sort[parent_num]={0,1,2,3,4,5};
+    int fitness[parent_num];
+    int fitness_sort[parent_num];
+    float total=0;
+    int fitness_total=0;
+
+//    cout<<"parent"<<endl;
+    //產生母群的SSE
+    for (int z = 0; z < parent_num; z++) {
+        center_find(parent,z);     //群中心
+        SSE_cal(z);         //計算SSE
+        total+=SSE[z];
+        Groupset.clear();
+
+//        for(int a=0;a <data_num;a++){
+//            cout<<setw(4)<<a<<"["<<parent[z][a]<<"] ";
+//        }
+//        cout << setprecision(5) << "[" << z << "] SSE=" << SSE[z] << endl;
+    }
+
+    for(int i=0;i<parent_num;i++){
+        fitness_sort[i]=(SSE[i]/total)*1000;
+    }
+
+    bubbleSortIncrease(SSE,index_sort,parent_num);
+    bubbleSortDecrease(fitness_sort,parent_num);
+
+    for(int i=0;i<parent_num;i++){
+        fitness[index_sort[i]]=fitness_sort[i];
+    }
+
+    for(int i=0;i<parent_num;i++){
+//        cout<<"["<<i<<"] fitness="<<fitness[i]<<endl;
         fitness_total+=fitness[i];
     }
 
@@ -206,31 +290,25 @@ void reproduction() {
 }
 
 void crossover(){
-    int rand_choice1 = rand()%parent_num;
-    int rand_choice2;
     int temp[data_num];
-    int count=0;
     for(int i=0;i<parent_num/2;i++) {
-        do {
-            rand_choice2 = rand() % parent_num;
-        } while (rand_choice2 == rand_choice1);
 
         int change_point = rand() % data_num;
-        if((double)(rand())/(RAND_MAX + 1.0)>0.05) {
+        if((double)(rand())/(RAND_MAX + 1.0)<crossover_rate) {
             for (int j = change_point; j < data_num; j++) {
-                temp[j] = choice[rand_choice1][j];
-                choice[rand_choice1][j] = choice[rand_choice2][j];
-                choice[rand_choice2][j] = temp[j];
+                temp[j] = choice[i * 2][j];
+                choice[i * 2][j] = choice[i * 2 + 1][j];
+                choice[i * 2 + 1][j] = temp[j];
             }
             for (int k = 0; k < data_num; k++) {
-                children[i * 2][k] = choice[rand_choice1][k];
-                children[i * 2 + 1][k] = choice[rand_choice2][k];
+                children[i * 2][k] = choice[i * 2][k];
+                children[i * 2 + 1][k] = choice[i * 2 + 1][k];
             }
         }
         else{
             for (int k = 0; k < data_num; k++) {
-                children[i * 2][k] = choice[rand_choice1][k];
-                children[i * 2 + 1][k] = choice[rand_choice2][k];
+                children[i * 2][k] = choice[i * 2][k];
+                children[i * 2 + 1][k] = choice[i * 2 + 1][k];
             }
         }
     }
@@ -245,16 +323,14 @@ void crossover(){
 }
 
 void mutation(){
-
     for(int i=0;i<parent_num;i++){
         for(int j=0;j<mutation_num;j++){
-            if((double)(rand())/(RAND_MAX + 1.0)>0.05){
+            if((double)(rand())/(RAND_MAX + 1.0)<mutation_rate){
                 int rand1=rand() % data_num;
                 children[i][rand1]=rand() % group_num;
             }
         }
     }
-
 
 //    for (int i = 0; i < parent_num; i++) {
 //        cout<<"Children "<<i<<endl;
@@ -273,6 +349,9 @@ void reset() {
 }
 
 int main() {
+    float min=9999;
+    int min_group[data_num];
+
     clock_t start, end;
     start = clock();
 
@@ -280,6 +359,34 @@ int main() {
     read_file();
 
     initial();      //初始化
+    crossover();    //交配
+    mutation();     //突變
+
+    cout << "children" << endl;
+    //產生子群的SSE
+
+//    for (int z = 0; z < parent_num; z++) {
+//        for(int a=0;a <data_num;a++){
+//            cout<<setw(4)<<a<<"["<<children[z][a]<<"] ";
+//        }
+//    }
+    for (int z = 0; z < parent_num; z++) {
+        center_find(children,z);     //群中心
+        SSE_cal(z);         //計算SSE
+        Groupset.clear();
+
+//          for(int a=0;a <data_num;a++){
+//                cout<<setw(4)<<a<<"["<<children[z][a]<<"] ";
+//          }
+        cout << setprecision(5) << "[" << z << "] SSE=" << SSE[z] << endl;
+        if(SSE[z]<min){
+            min=SSE[z];
+            for(int i=0;i<data_num;i++){
+                min_group[i]=children[z][i];
+            }
+        }
+    }
+    reset();
 
     for(int i=0;i<circulartimes;i++) {
         reproduction(); //選擇
@@ -287,14 +394,22 @@ int main() {
         mutation();     //突變
 
         cout << "children" << endl;
-        for (int z = 0; z < parent_num; z++) {     //產生子群的SSE
-            center_find(children, z);     //群中心
+        //產生子群的SSE
+        for (int z = 0; z < parent_num; z++) {
+            center_find(children,z);     //群中心
             SSE_cal(z);         //計算SSE
-
+            Groupset.clear();
 //          for(int a=0;a <data_num;a++){
 //                cout<<setw(4)<<a<<"["<<children[z][a]<<"] ";
 //          }
             cout << setprecision(5) << "[" << z << "] SSE=" << SSE[z] << endl;
+
+            if(SSE[z]<min){
+                min=SSE[z];
+                for(int i=0;i<data_num;i++){
+                    min_group[i]=children[z][i];
+                }
+            }
         }
 
         if(i!=circulartimes-1) {
@@ -302,6 +417,10 @@ int main() {
         }
     }
 
+    cout<<"minimal SSE= "<<min<<endl;
+    for(int i=0;i<data_num;i++){
+        cout<<setw(4)<<i<<"["<<min_group[i]<<"] ";
+    }
 
     end = clock();
     double diff = end - start; // ms
